07aec70996fd54ffd83dc96a6126476fe1c55a14
GooseCampagne <gustavodante202@gmail.com>
2023-03-10 12:56:25 -0600

    v2

--- src/App.js
+++ src/App.js
@@ -20,6 +20,8 @@ function App() {
      {/* <Contacto /> 
      <Footer />  */}
      <Whatsapp/>
+     {/* <Contacto /> 
+     <Footer />  */}
     </div>
 
   );
---
+++ src/components/Whatsapp.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { FloatingWhatsApp } from 'react-floating-whatsapp'
+
+export default function Whatsapp() {
+
+  return (
+      <FloatingWhatsApp  
+        phoneNumber="6183564435"
+        accountName="Mi chuy"
+        allowEsc
+        allowClickAway
+        notification
+        notificationSound 
+        statusMessage="Responde en una hora"
+        chatMessage="Hola, tacos y hamburguesas mi chuy lo atiende"
+        avatar="./logo.png"/>
+  )
+}
